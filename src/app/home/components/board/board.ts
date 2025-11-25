9cfecf418749da2053994b8902521a229e7b595c
KravchenkoMaks <maviegma@gmail.com>
2025-11-25 02:33:33 +0200

    update board title - signal

--- src/app/home/components/board/board.ts
+++ src/app/home/components/board/board.ts
@@ -1,75 +1,60 @@
-import { ChangeDetectionStrategy, Component, ElementRef, inject, signal, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { List } from '../list/list';
 import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
 import { IBoard } from '../../interfaces/i-board';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { SBoards } from '../../services/s-boards';
+import { BoardHeader } from '../board-header/board-header';
 
 @Component({
   selector: 'tr-board',
-  imports: [RouterOutlet, List, RouterLink, ReactiveFormsModule],
+  imports: [RouterOutlet, List, RouterLink, ReactiveFormsModule, BoardHeader],
   templateUrl: './board.html',
   styleUrl: './board.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Board {
-  @ViewChild('inputRef') inputRef!: ElementRef;
   private route = inject(ActivatedRoute);
-  private fb = inject(FormBuilder);
   private boardsService = inject(SBoards);
-  protected form: FormGroup;
-  board?: IBoard;
-  private boardId!: number;
-  isEditing = signal<boolean>(false);
+  private fb = inject(FormBuilder);
+
+  form: FormGroup;
+  board = signal<IBoard | undefined>(undefined);
+  private boardId: number;
   private saveTriggered = false;
 
   constructor() {
-    this.boardId = Number(this.route.snapshot.paramMap.get('id'));
-
-    this.route.data.pipe(takeUntilDestroyed()).subscribe(({ board }) => {
-      this.board = { ...board, id: this.boardId };
-    });
     this.form = this.fb.group({
       title: ['', [Validators.required, Validators.pattern(/^(?!.*[эЭёЁ])[a-zA-Zа-яА-ЯїЇіІєЄґҐ0-9 .\-_]+$/)]],
     });
+
+    this.boardId = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.route.data.pipe(takeUntilDestroyed()).subscribe(({ board }) => {
+      console.log(board);
+      this.board.set({ ...board, id: this.boardId });
+    });
   }
 
-  protected enableEdit() {
-    this.isEditing.set(true);
-    const boardTitle = this.board?.title.trim() ?? '';
-    this.form.patchValue({ title: boardTitle });
-    setTimeout(() => this.inputRef?.nativeElement.focus());
-  }
-
-  protected saveEdit() {
+  updateTitle(newTitle: string) {
     if (this.saveTriggered || this.form.invalid) {
       return;
     }
     this.saveTriggered = true;
     setTimeout(() => (this.saveTriggered = false), 500);
 
-    const id = this.board?.id ?? this.boardId;
-    const newTitle = this.form.value.title.trim();
+    const id = this.board()?.id ?? this.boardId;
 
-    console.log('end: ');
     this.boardsService.updateBoard(id, { title: newTitle }).subscribe({
       next: () => {
         this.boardsService.getBoard(id).subscribe({
-          next: (updatedBoard) => {
-            this.board = { ...updatedBoard, id };
-            this.isEditing.set(false);
-          },
-          error: (err) => {
-            console.error('Помилка при отриманні оновленої дошки:', err);
-            this.isEditing.set(false);
-          },
+          next: (updatedBoard) => this.board.set({ ...updatedBoard, id }),
+          error: (err) => console.error('Помилка при GET:', err),
         });
       },
-      error: (err) => {
-        console.error('Помилка при оновленні назви:', err);
-        this.isEditing.set(false);
-      },
+      error: (err) => console.error('Помилка при PUT:', err),
     });
+    console.log('gggg');
   }
 }
