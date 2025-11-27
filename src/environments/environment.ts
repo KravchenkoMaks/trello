79afb1c46c59b9dee8e2c7f4f488fdd531fbedd2
KravchenkoMaks <maviegma@gmail.com>
2025-11-27 11:10:41 +0200

    ch gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  production: false,
+  production: true,
   baseURL: 'https://trello-back.shpp.me/mkravchenko/api/v1',
-  // baseURL: 'https://trello-back.shpp.me/odavydiuk/api/v1',
 };
